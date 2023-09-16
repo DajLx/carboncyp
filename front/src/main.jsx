2b8bad915271749f94fde547854dbc43aa8962dd
alex <durandaj027@gmail.com>
2023-09-16 15:10:11 -0400

    fifth deploy

--- front/src/main.jsx
+++ front/src/main.jsx
@@ -3,16 +3,15 @@ import ReactDOM from "react-dom/client";
 import App from "./App.js";
 import "./index.css";
 import { BrowserRouter } from "react-router-dom";
-import { Provider } from "react-redux"
+import { Provider } from "react-redux";
 import store from "./state/store.js";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-
   <BrowserRouter>
     <React.StrictMode>
-      <Provider store={store}><App /></Provider>
-
+      <Provider store={store}>
+        <App />
+      </Provider>
     </React.StrictMode>
-
   </BrowserRouter>
 );
